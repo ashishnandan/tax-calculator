357c14b0585e11e990dbabc79c1e5c24c0f093ac
ashishnandan <aashiana6740>
2020-06-18 23:49:00 +0530

    Add get calculated tax without cess

--- src/main/scala/tax/calculator/service/TaxCalculatorService.scala
+++ src/main/scala/tax/calculator/service/TaxCalculatorService.scala
@@ -1,5 +1,6 @@
 package tax.calculator.service
 
+import tax.calculator.TaxCalculator
 import tax.calculator.input.FileReader
 import tax.calculator.model.TaxCalculatorItem
 
@@ -10,19 +11,35 @@ class TaxCalculatorService(year: Int, age: Int, income: Double, investment: Doub
   val senior_citizen_year_wise_rebate: Map[Int, Int] = Map(2018 -> 0, 2019 -> 50000, 2020 -> 75000)
   var tax_brackets: List[TaxCalculatorItem] = FileReader.read(year + ".csv")
 
-  def get_tax_slab(): Int = {
-    var counter: Int = 1
+  def get_tax_slab: Int = {
     val taxable_inc: Double = income - get_deduction_amount(age, year, investment)
+    var counter: Int = 1
     breakable {
       tax_brackets.foreach(tb => if (tb.max_inc_range >= taxable_inc || tb.max_inc_range == 0) break else counter += 1)
     }
     counter
   }
 
-  def get_deduction_amount(age: Int, year: Int, investment: Double): Double = {
+  private def get_deduction_amount(age: Int, year: Int, investment: Double): Double = {
     val is_senior_citizen: Boolean = age >= 60
     val senior_citizen_amount: Double = if (is_senior_citizen) senior_citizen_year_wise_rebate(year) else 0
     investment + senior_citizen_amount
   }
 
+  def get_tax: Double = {
+    val tax_slab: Double = get_tax_slab
+    var counter: Int = 0
+    var tax: Double = 0
+    var taxable_inc: Double = income - get_deduction_amount(age, year, investment)
+    for (tax_bracket <- tax_brackets) {
+      if (counter < tax_slab) {
+        val amount: Double = if (tax_bracket.max_inc_range == 0) taxable_inc else tax_bracket.max_inc_range - tax_bracket.min_inc_range + 1
+        val min_amount_to_deduct: Double = Math.min(taxable_inc, amount)
+        tax += TaxCalculator(min_amount_to_deduct, tax_bracket.tax_percentage).taxable_amount()
+        taxable_inc = taxable_inc - amount
+        counter += 1
+      }
+    }
+    tax
+  }
 }
