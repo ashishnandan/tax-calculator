04a64f99170aceba284a8901995bacbd49352f6a
ashishnandan <aashiana6740>
2020-06-19 10:20:30 +0530

    Add cess if taxable amount is greater than 500000

--- src/main/scala/tax/calculator/service/TaxCalculatorService.scala
+++ src/main/scala/tax/calculator/service/TaxCalculatorService.scala
@@ -10,10 +10,11 @@ class TaxCalculatorService(year: Int, age: Int, income: Double, investment: Doub
 
   val senior_citizen_year_wise_rebate: Map[Int, Int] = Map(2018 -> 0, 2019 -> 50000, 2020 -> 75000)
   val year_wise_cess_rate: Map[Int, Double] = Map(2018 -> 1, 2019 -> 2, 2020 -> 5)
+  val min_tax_for_cess: Double = 500000
   var tax_brackets: List[TaxCalculatorItem] = FileReader.read(year + ".csv")
 
-  def get_tax_slab: Int = {
-    val taxable_inc: Double = income - get_deduction_amount(age, year, investment)
+  def taxSlab: Int = {
+    val taxable_inc: Double = income - getDeductionAmount(age, year, investment)
     var counter: Int = 1
     breakable {
       tax_brackets.foreach(tb => if (tb.max_inc_range >= taxable_inc || tb.max_inc_range == 0) break else counter += 1)
@@ -21,17 +22,17 @@ class TaxCalculatorService(year: Int, age: Int, income: Double, investment: Doub
     counter
   }
 
-  private def get_deduction_amount(age: Int, year: Int, investment: Double): Double = {
+  private def getDeductionAmount(age: Int, year: Int, investment: Double): Double = {
     val is_senior_citizen: Boolean = age >= 60
     val senior_citizen_amount: Double = if (is_senior_citizen) senior_citizen_year_wise_rebate(year) else 0
     investment + senior_citizen_amount
   }
 
-  def get_tax: Double = {
-    val tax_slab: Double = get_tax_slab
+  def calculateTax: Double = {
+    val tax_slab: Double = taxSlab
     var counter: Int = 0
     var tax: Double = 0
-    var taxable_inc: Double = income - get_deduction_amount(age, year, investment)
+    var taxable_inc: Double = income - getDeductionAmount(age, year, investment)
     for (tax_bracket <- tax_brackets) {
       if (counter < tax_slab) {
         val amount: Double = if (tax_bracket.max_inc_range == 0) taxable_inc else tax_bracket.max_inc_range - tax_bracket.min_inc_range + 1
@@ -41,7 +42,7 @@ class TaxCalculatorService(year: Int, age: Int, income: Double, investment: Doub
         counter += 1
       }
     }
-    if (tax > 500000) tax += tax*(year_wise_cess_rate(year)/100)
+    if (tax > min_tax_for_cess) tax += tax * (year_wise_cess_rate(year) / 100)
     tax
   }
 }
--- src/test/scala/tax/calculator/service/TaxCalculatorServiceTest.scala
+++ src/test/scala/tax/calculator/service/TaxCalculatorServiceTest.scala
@@ -8,38 +8,38 @@ import org.scalatestplus.junit.JUnitRunner
 class TaxCalculatorServiceTest extends FunSuite{
 
   test("get Tax slab for a valid input") {
-    assert(3 == new TaxCalculatorService(2018, 30, 750000, 0).get_tax_slab)
-    assert(4 == new TaxCalculatorService(2018, 30, 1250009, 0).get_tax_slab)
-    assert(1 == new TaxCalculatorService(2018, 30, 75000, 0).get_tax_slab)
-    assert(2 == new TaxCalculatorService(2018, 30, 250000, 0).get_tax_slab)
+    assert(3 == new TaxCalculatorService(2018, 30, 750000, 0).taxSlab)
+    assert(4 == new TaxCalculatorService(2018, 30, 1250009, 0).taxSlab)
+    assert(1 == new TaxCalculatorService(2018, 30, 75000, 0).taxSlab)
+    assert(2 == new TaxCalculatorService(2018, 30, 250000, 0).taxSlab)
 
-    assert(3 == new TaxCalculatorService(2019, 30, 750000, 0).get_tax_slab)
-    assert(2 == new TaxCalculatorService(2019, 30, 125000, 0).get_tax_slab)
-    assert(4 == new TaxCalculatorService(2019, 30, 7500000, 0).get_tax_slab)
-    assert(2 == new TaxCalculatorService(2019, 30, 550000, 0).get_tax_slab)
+    assert(3 == new TaxCalculatorService(2019, 30, 750000, 0).taxSlab)
+    assert(2 == new TaxCalculatorService(2019, 30, 125000, 0).taxSlab)
+    assert(4 == new TaxCalculatorService(2019, 30, 7500000, 0).taxSlab)
+    assert(2 == new TaxCalculatorService(2019, 30, 550000, 0).taxSlab)
 
-    assert(2 == new TaxCalculatorService(2019, 60, 625000, 0).get_tax_slab)
-    assert(1 == new TaxCalculatorService(2019, 60, 125000, 0).get_tax_slab)
-    assert(4 == new TaxCalculatorService(2019, 60, 7500000, 0).get_tax_slab)
-    assert(2 == new TaxCalculatorService(2019, 60, 650000, 0).get_tax_slab)
-    assert(3 == new TaxCalculatorService(2019, 40, 650000, 0).get_tax_slab)
+    assert(2 == new TaxCalculatorService(2019, 60, 625000, 0).taxSlab)
+    assert(1 == new TaxCalculatorService(2019, 60, 125000, 0).taxSlab)
+    assert(4 == new TaxCalculatorService(2019, 60, 7500000, 0).taxSlab)
+    assert(2 == new TaxCalculatorService(2019, 60, 650000, 0).taxSlab)
+    assert(3 == new TaxCalculatorService(2019, 40, 650000, 0).taxSlab)
 
-    assert(2 == new TaxCalculatorService(2020, 60, 1275000, 200000).get_tax_slab)
-    assert(3 == new TaxCalculatorService(2020, 50, 1275000, 200000).get_tax_slab)
+    assert(2 == new TaxCalculatorService(2020, 60, 1275000, 200000).taxSlab)
+    assert(3 == new TaxCalculatorService(2020, 50, 1275000, 200000).taxSlab)
   }
 
   test("get income tax for valid input") {
-    assert(90000 == new TaxCalculatorService(2018, 30, 750000, 0).get_tax)
-    assert(215002.7 == new TaxCalculatorService(2018, 30, 1250009, 0).get_tax)
-    assert(0 == new TaxCalculatorService(2018, 30, 75000, 0).get_tax)
-    assert(15000 == new TaxCalculatorService(2018, 30, 250000, 0).get_tax)
+    assert(90000 == new TaxCalculatorService(2018, 30, 750000, 0).calculateTax)
+    assert(215002.7 == new TaxCalculatorService(2018, 30, 1250009, 0).calculateTax)
+    assert(0 == new TaxCalculatorService(2018, 30, 75000, 0).calculateTax)
+    assert(15000 == new TaxCalculatorService(2018, 30, 250000, 0).calculateTax)
 
-    assert(2040000 == new TaxCalculatorService(2019, 66, 7500000, 150000).get_tax)
-    assert(2055300 == new TaxCalculatorService(2019, 56, 7500000, 150000).get_tax)
-    assert(140000 == new TaxCalculatorService(2019, 66, 1250000, 150000).get_tax)
+    assert(2040000 == new TaxCalculatorService(2019, 66, 7500000, 150000).calculateTax)
+    assert(2055300 == new TaxCalculatorService(2019, 56, 7500000, 150000).calculateTax)
+    assert(140000 == new TaxCalculatorService(2019, 66, 1250000, 150000).calculateTax)
 
-    assert(1848000 == new TaxCalculatorService(2020, 56, 7500000, 0).get_tax)
-    assert(1788937.5 == new TaxCalculatorService(2020, 66, 7500000, 150000).get_tax)
-    assert(1775812.5 == new TaxCalculatorService(2020, 66, 7500000, 200000).get_tax)
+    assert(1848000 == new TaxCalculatorService(2020, 56, 7500000, 0).calculateTax)
+    assert(1788937.5 == new TaxCalculatorService(2020, 66, 7500000, 150000).calculateTax)
+    assert(1775812.5 == new TaxCalculatorService(2020, 66, 7500000, 200000).calculateTax)
   }
 }
